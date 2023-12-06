1f36217c2c544977a7bd1e2a0a84aff99d3f8412
Syed Murtaza Hussain <sirmurtazaaptechtr@outlook.com>
2023-12-06 19:19:09 +0500

    04-Dec-2023

---
+++ PWJS/4-Dec-2023/main.js
@@ -0,0 +1,22 @@
+const myTable = document.getElementById('myTable');
+const request = new Request('catlog.json');
+
+fetch(request)
+.then(function (response) {
+    return response.json();
+})
+.then(function(catlog){    
+    let plants = catlog.Plants;    
+    plants.forEach(function (plant,index) {        
+        myTable.innerHTML += 
+        `<tr>
+            <th>${index+1}</th>
+            <td>${plant.Common}</td>
+            <td>${plant.Botanical}</td>
+            <td>${plant.Zone}</td>
+            <td>${plant.Light}</td>
+            <td>${plant.Price}</td>
+            <td>${plant.Availability}</td>
+        </tr>`
+    });
+});
